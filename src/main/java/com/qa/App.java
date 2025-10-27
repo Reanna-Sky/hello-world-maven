2834d6012b52ce4e6f8d32e2bb16278a21715829
Reanna-Sky <reannagibsonsky@gmail.com>
2025-10-27 15:03:55 +0000

    testing if it all works

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Reanna"));
+        System.out.println(sayHelloToSomeone("Testing"));
         System.out.println(sayGoodbye());
     }
 
